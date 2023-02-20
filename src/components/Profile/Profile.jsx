82488a1bafe271fea78765246ec87a47fe4a5074
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-02-20 19:49:18 +0200

    complete Task1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+
+
+// username — ім'я користувача
+// tag — тег в соціальній мережі без @
+// location — місто і країна
+// avatar — посилання на зображення
+// stats — об'єкт з інформацією про активності
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  const { followers, views, likes } = stats;
+  return (
+    <div className="profile">
+    <div className="description">
+      <img
+        src={avatar}
+        alt="User avatar"
+        className="avatar"
+      />
+      <p className="name">{username}</p>
+      <p className="tag">@{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{likes}</span>
+      </li>
+    </ul>
+  </div>
+  )
+}
+
+Profile.propTypes ={
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  })
+}
+
